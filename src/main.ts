f361d13c93363047de62acea8f44a2b14bd1d7ce
Bailey <hello@bailey.sh>
2021-09-09 05:41:00 -0700

    Move to TypeScript (#14)

---
+++ src/main.ts
@@ -0,0 +1,260 @@
+import fs from 'fs';
+import path from 'path';
+
+/**
+ * The native addon binding.
+ */
+const nativeAddon = (function() {
+	const gyp = require('@mapbox/node-pre-gyp');
+	const packagePath = path.resolve(path.join(__dirname, '../package.json'));
+	const addonPath: string = gyp.find(packagePath);
+
+	if (!fs.existsSync(addonPath)) {
+		throw new Error(
+			'Could not find the "utimes.node" file. See: https://github.com/baileyherbert/utimes/issues/12'
+		);
+	}
+
+	return require(addonPath);
+})();
+
+/**
+ * Whether or not the current platform supports the native addon.
+ */
+const useNativeAddon = ['darwin', 'win32', 'linux'].indexOf(process.platform) >= 0;
+
+/**
+ * Updates the timestamps on the given file path(s).
+ *
+ * If a `callback` function is provided, it will be invoked after the operation completes with an optional error
+ * argument. Otherwise, a promise will be returned.
+ *
+ * @param path
+ * @param options
+ * @param callback
+ */
+export function utimes(path: string | string[], options: TimeOptions): Promise<void>;
+export function utimes(path: string | string[], options: TimeOptions, callback: (error?: Error) => void): void;
+export function utimes(path: string | string[], options: TimeOptions, callback?: (error?: Error) => void) {
+	return new Promise<void>((promiseResolve, promiseReject) => {
+		const targets = getNormalizedPaths(path);
+		const times = getNormalizedOptions(options);
+		const flags = getFlags(times);
+
+		const resolve = () => typeof callback === 'function' ? callback() : promiseResolve();
+		const reject = (error: Error) => typeof callback === 'function' ? callback(error) : promiseReject(error);
+		const invokeAtIndex = (index: number) => {
+			const target = targets[index];
+			if (target === undefined) {
+				return resolve();
+			}
+
+			// Invoke the native addon on supported platforms
+			if (useNativeAddon) {
+				invokeNativeAddon(
+					target,
+					times,
+					flags,
+					error => error !== undefined ? reject(error) : invokeAtIndex(index + 1)
+				);
+			}
+
+			// Fall back to using `fs.utimes` for other platforms
+			else {
+				fs.stat(target, (statsErr, stats) => {
+					if (statsErr) return reject(statsErr);
+
+					fs.utimes(
+						target,
+						(flags & 4 ? times.atime : stats.atime.getTime()) / 1000,
+						(flags & 2 ? times.mtime : stats.mtime.getTime()) / 1000,
+						error => error ? reject(error) : invokeAtIndex(index + 1)
+					);
+				});
+			}
+		};
+
+		// Return if there's nothing to do
+		if (!flags || !targets.length) {
+			return resolve();
+		}
+
+		// Start setting timestamps
+		invokeAtIndex(0);
+	});
+}
+
+/**
+ * Converts the given string or string array into a guaranteed array of strings.
+ *
+ * @param paths
+ */
+function getNormalizedPaths(paths: string | string[]): string[] {
+	if (typeof paths === 'string') {
+		assertPath('path', paths);
+		return [ paths ];
+	}
+
+	if (Array.isArray(paths)) {
+		for (let i = 0; i < paths.length; i++) {
+			const path = paths[i];
+			assertPath(`paths[${i}]`, path);
+		}
+
+		return paths;
+	}
+
+	throw new Error('path must be a string or array');
+}
+
+/**
+ * Replaces missing options with zero values.
+ *
+ * @param options
+ */
+function getNormalizedOptions(options: TimeOptions): NormalizedTimeOptions {
+	if (typeof options === 'number') {
+		options = {
+			btime: options,
+			mtime: options,
+			atime: options
+		};
+	}
+
+	if (typeof options === 'undefined' || options === null) {
+		options = {
+			btime: 0,
+			mtime: 0,
+			atime: 0
+		};
+	}
+
+	if (typeof options !== 'object') {
+		throw new Error('options must be an object');
+	}
+
+	assertTimestamp('btime', options.btime);
+	assertTimestamp('mtime', options.mtime);
+	assertTimestamp('atime', options.atime);
+
+	return {
+		btime: options.btime || 0,
+		mtime: options.mtime || 0,
+		atime: options.atime || 0
+	};
+}
+
+/**
+ * Calculates the flags to send to the binding.
+ *
+ * @param options
+ */
+function getFlags(options: NormalizedTimeOptions): number {
+	let flags = 0;
+
+	if (options.btime) flags |= 1;
+	if (options.mtime) flags |= 2;
+	if (options.atime) flags |= 4;
+
+	return flags;
+}
+
+/**
+ * Calls the binding and invokes a callback function.
+ *
+ * @param path
+ * @param times
+ * @param flags
+ */
+function invokeNativeAddon(path: string, times: NormalizedTimeOptions, flags: number, callback: (error?: Error) => void): void {
+	nativeAddon.utimes(getPathBuffer(path), flags, times.btime, times.mtime, times.atime, (result: number) => {
+		if (result !== 0) {
+			return callback(new Error(`(${result}), utimes(${path})`));
+		}
+
+		callback();
+	});
+}
+
+/**
+ * Converts a path string into a buffer.
+ *
+ * @param target
+ * @returns
+ */
+function getPathBuffer(target: string) {
+	const targetLong = (path as any)._makeLong(target);
+	const buffer = Buffer.alloc(Buffer.byteLength(targetLong, 'utf-8') + 1);
+
+	buffer.write(targetLong, 0, buffer.length - 1, 'utf-8');
+	buffer[buffer.length - 1] = 0;
+
+	if (buffer.indexOf(0) !== buffer.length - 1) {
+		throw new Error('path must be a string without null bytes');
+	}
+
+	return buffer;
+}
+
+/**
+ * @param key
+ * @param value
+ */
+function assertPath(key: string, value: any) {
+	if (typeof value !== 'string') {
+		throw new Error(key + ' must be a string');
+	}
+
+	if (value.length === 0) {
+		throw new Error(key + ' must not be empty');
+	}
+
+	if (value.indexOf('\u0000') !== -1) {
+		throw new Error(key + ' must be a string without null bytes');
+	}
+}
+
+function assertTimestamp(key: string, value: any) {
+	if (value === undefined) {
+		return;
+	}
+
+	if (typeof value !== 'number') {
+		throw new Error(key + ' must be a number or undefined');
+	}
+
+	if (Math.floor(value) !== value) {
+		throw new Error(key + ' must be an integer');
+	}
+
+	if (value < 0) {
+		throw new Error(key + ' must be a positive integer');
+	}
+
+	if (value > Math.pow(2, 48) - 1) {
+		throw new Error(key + ' must not be more than ' + (Math.pow(2, 48) - 1));
+	}
+}
+
+/**
+ * Options for choosing which timestamps to set on files. You can supply a single number to set that as the
+ * timestamp for all three, or supply individual timestamps within an object.
+ */
+export type TimeOptions = number | null | undefined | Partial<NormalizedTimeOptions>;
+
+type NormalizedTimeOptions = {
+	/**
+	 * The birth time in milliseconds.
+	 */
+	btime: number;
+
+	/**
+	 * The modification time in milliseconds.
+	 */
+	mtime: number;
+
+	/**
+	 * The access time in milliseconds.
+	 */
+	atime: number;
+};
---
+++ tests/utils.ts
@@ -0,0 +1,204 @@
+import { utimes } from '../src/main';
+import fs from 'fs';
+import assert from 'assert';
+import util from 'util';
+
+/**
+ * Returns the timestamps for the given file.
+ *
+ * @param filePath
+ * @returns
+ */
+export function getFileTimes(filePath: string): ResolvedTimestampCollection {
+	const stats = fs.statSync(filePath);
+
+	return {
+		atime: stats.atime.getTime(),
+		btime: stats.birthtime.getTime(),
+		mtime: stats.mtime.getTime()
+	};
+}
+
+/**
+ * Asserts that the given file's timestamps match the given times.
+ *
+ * @param filePath
+ * @param expected
+ */
+export function assertFileTimes(filePath: string, expected: UTimestampCollection) {
+	const actual = getFileTimes(filePath);
+
+	if (typeof expected.atime !== 'undefined') {
+		assert.equal(actual.atime, expected.atime, getTimeMismatchMessage('atime', actual, expected));
+	}
+
+	if (typeof expected.btime !== 'undefined' && process.platform !== 'linux') {
+		assert.equal(actual.btime, expected.btime, getTimeMismatchMessage('btime', actual, expected));
+	}
+
+	if (typeof expected.mtime !== 'undefined') {
+		assert.equal(actual.mtime, expected.mtime, getTimeMismatchMessage('mtime', actual, expected));
+	}
+}
+
+/**
+ * Asserts that the given file's timestamps match (or are relatively close to) the given times.
+ *
+ * @param filePath
+ * @param expected
+ * @param margin Milliseconds
+ */
+export function assertFileTimesLoosely(filePath: string, expected: ResolvedTimestampCollection, margin = 10) {
+	const actual = getFileTimes(filePath);
+	const diff = (a: number, b: number) => Math.abs(a - b);
+
+	assert(diff(actual.atime, expected.atime) <= margin, getTimeMismatchMessage('atime', actual, expected));
+	assert(diff(actual.mtime, expected.mtime) <= margin, getTimeMismatchMessage('mtime', actual, expected));
+
+	if (process.platform !== 'linux') {
+		assert(diff(actual.btime, expected.btime) <= margin, getTimeMismatchMessage('btime', actual, expected));
+	}
+}
+
+/**
+ * Returns an error message to use for a timestamp mismatch.
+ *
+ * @param name
+ * @param actual
+ * @param expected
+ */
+export function getTimeMismatchMessage(name: 'atime' | 'btime' | 'mtime', actual: UTimestampCollection, expected: UTimestampCollection) {
+	return util.format(
+		'Incorrect timestamp for %s, expected %s, got %s',
+		name,
+		JSON.stringify(expected),
+		JSON.stringify(actual)
+	);
+}
+
+/**
+ * Returns the current timestamp as an object with `btime`, `mtime`, and `atime`.
+ *
+ * @returns
+ */
+export function getNow(): UTimestampCollection {
+	const now = (new Date()).getTime();
+
+	return {
+		atime: now,
+		btime: now,
+		mtime: now
+	};
+}
+
+/**
+ * Returns the current timestamp as an object with `btime`, `mtime`, and `atime`.
+ *
+ * @param a
+ * @param b
+ * @returns
+ */
+export function mergeTimes(a: UTimestampCollection, b: UTimestampCollection | number): UTimestampCollection {
+	if (typeof b === 'number') {
+		b = {
+			mtime: b,
+			atime: b,
+			btime: b
+		};
+	}
+
+	return Object.assign(a, b);
+}
+
+/**
+ * Returns the current timestamp as an object with `btime`, `mtime`, and `atime`.
+ *
+ * @param filePath
+ * @param times
+ * @returns
+ */
+export async function testSetTimes(filePath: string, times: UTimestampCollection | number) {
+	const now = getFileTimes(filePath);
+	const expected = mergeTimes(now, times);
+
+	await utimes(filePath, times);
+	assertFileTimes(filePath, expected);
+}
+
+/**
+ * Returns the current timestamp as an object with `btime`, `mtime`, and `atime`.
+ *
+ * @param filePath
+ * @param times
+ * @returns
+ */
+export async function testSetTimesCallback(filePath: string, times: UTimestampCollection | number) {
+	return new Promise<void>((resolve, reject) => {
+		const now = getFileTimes(filePath);
+		const expected = mergeTimes(now, times);
+
+		utimes(filePath, times, error => {
+			if (error) return reject(error);
+
+			assertFileTimes(filePath, expected);
+			resolve();
+		});
+	});
+}
+
+/**
+ * Returns the current timestamp as an object with `btime`, `mtime`, and `atime`.
+ *
+ * @param filePath
+ * @param times
+ * @returns
+ */
+export async function testSetTimesMulti(filePaths: string[], times: UTimestampCollection | number) {
+	const targets = filePaths.map(path => ({ path, expected: mergeTimes(getFileTimes(path), times) }));
+	await utimes(filePaths, times);
+
+	for (const file of targets) {
+		assertFileTimes(file.path, file.expected);
+	}
+}
+
+/**
+ * Returns the current timestamp as an object with `btime`, `mtime`, and `atime`.
+ *
+ * @param filePath
+ * @param times
+ * @returns
+ */
+export async function testSetTimesMultiCallback(filePaths: string[], times: UTimestampCollection | number) {
+	return new Promise<void>((resolve, reject) => {
+		const targets = filePaths.map(path => ({ path, expected: mergeTimes(getFileTimes(path), times) }));
+		utimes(filePaths, times, error => {
+			if (error) return reject(error);
+
+			for (const file of targets) {
+				assertFileTimes(file.path, file.expected);
+			}
+
+			resolve();
+		});
+	});
+}
+
+export type ResolvedTimestampCollection = {
+	/**
+	 * The birth time in milliseconds.
+	 */
+	btime: number;
+
+	/**
+	 * The modification time in milliseconds.
+	 */
+	mtime: number;
+
+	/**
+	 * The access time in milliseconds.
+	 */
+	atime: number;
+};
+
+export type UTimestampCollection = Partial<ResolvedTimestampCollection>;
